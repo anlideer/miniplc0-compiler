a3001b92cada425f24f932737241d32c34bf8850
anlideer <643525796@qq.com>
2019-11-21 07:02:16 -0800

    update test

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -17,7 +17,20 @@ TEST_CASE("Test hello world.", "basic") {
 	std::stringstream ss;
 	ss.str(input);
 	miniplc0::Tokenizer tkz(ss);
-	std::vector<miniplc0::Token> output = {};
+	std::vector<miniplc0::Token> output = {
+		"Line: 0 Column: 0 Type: Begin Value: begin\n
+		Line: 1 Column: 1 Type: Var Value: var\n
+		Line: 1 Column: 5 Type: Identifier Value: a\n
+		Line: 1 Column: 7 Type: EqualSign Value: =\n
+		Line: 1 Column: 9 Type: UnsignedInteger Value: 1\n
+		Line: 1 Column: 10 Type: Semicolon Value: ;\n
+		Line: 2 Column: 1 Type: Print Value: print\n
+		Line: 2 Column: 6 Type: LeftBracket Value: (\n
+		Line: 2 Column: 7 Type: Identifier Value: a\n
+		Line: 2 Column: 8 Type: RightBracket Value: )\n
+		Line: 2 Column: 9 Type: Semicolon Value: ;\n
+		Line: 3 Column: 0 Type: End Value: end\n"
+	};
 	auto result = tkz.AllTokens();
 	if (result.second.has_value()) {
 		FAIL();
@@ -26,6 +39,7 @@ TEST_CASE("Test hello world.", "basic") {
 	
 }
 
+/*
 // try 1 try
 TEST_CASE("Test redefine.", "basic")
 {
@@ -39,10 +53,13 @@ TEST_CASE("Test redefine.", "basic")
 	std::stringstream ss;
 	ss.str(input);
 	miniplc0::Tokenizer tkz(ss);
-	std::vector<miniplc0::Token> output = {};
+	std::vector<miniplc0::Token> output = {
+
+	};
 	auto result = tkz.AllTokens();
 	if (result.second.has_value()) {
 		FAIL();
 	}
 	REQUIRE( (result.first == output) );
 }
+*/
